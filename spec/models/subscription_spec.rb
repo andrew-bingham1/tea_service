c6047d699c8b0dd9ae4f58967df43c7394bc10a3
Andrew Bingham <andrewgbingham@gmail.com>
2023-08-01 11:35:53 -0600

    test: create model tests

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Subscription, type: :model do
+  describe 'relationships' do
+    it { should have_many :customer_subscriptions }
+    it { should have_many(:customers).through(:customer_subscriptions) }
+    it { should have_many :tea_subscriptions }
+    it { should have_many(:teas).through(:tea_subscriptions) }
+  end
+end
