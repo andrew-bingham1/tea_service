cb0b1672b7627a663bf065e6e06b2b846a361f02
Andrew Bingham <andrewgbingham@gmail.com>
2023-08-01 13:22:59 -0600

    fix: update customer sub spec and .gitignore

--- spec/models/customer_subscription_spec.rb
+++ spec/models/customer_subscription_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe CustomerSubscription, type: :model do
     it { should belong_to :customer }
     it { should belong_to :subscription }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :customer_id }
+    it { should validate_presence_of :subscription_id }
+  end
 end
