57845fa202b8dc1fed303fe351b0f5f384cea5eb
Andrew Bingham <andrewgbingham@gmail.com>
2023-08-01 13:21:19 -0600

    feat: add route for creation of subscription

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v0 do
+      resources :customer_subscription, only: [:create]
+    end
+  end
 end
