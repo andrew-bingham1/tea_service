63ff95a9ed5dcfbae5a3217bf541b0f8d7a43a27
Andrew Bingham <andrewgbingham@gmail.com>
2023-08-01 11:01:00 -0600

    feat: set up db and rspec

---
+++ db/migrate/20230801165048_create_subscription.rb
@@ -0,0 +1,11 @@
+class CreateSubscription < ActiveRecord::Migration[7.0]
+  def change
+    create_table :subscriptions do |t|
+      t.string :title
+      t.float :price
+      t.integer :frequency
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20230801165608_create_tea.rb
@@ -0,0 +1,10 @@
+class CreateTea < ActiveRecord::Migration[7.0]
+  def change
+    create_table :teas do |t|
+      t.string :title
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
