0327d3d89e608cd77c9e8144c3326ae1bb1666b6
Andrew Bingham <andrewgbingham@gmail.com>
2023-08-03 08:11:24 -0600

    test: create tests for customer model instance methods

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,4 +5,36 @@ RSpec.describe Customer, type: :model do
     it { should have_many :customer_subscriptions }
     it { should have_many(:subscriptions).through(:customer_subscriptions) }
   end
+
+  describe 'instance methods' do
+    it 'can give active subscriptions' do
+      customer_1 = Customer.create!(first_name: 'Jim', last_name: 'Bob', email: 'jimmmybob@email.com', address: 'Springfield')
+      subscription_1 = Subscription.create!(title: 'Monthly', price: 10, frequency: 1)
+
+      expect(customer_1.active_subscriptions.count).to eq(0)
+
+      customer_subscription_1 = CustomerSubscription.create!(customer_id: customer_1.id, subscription_id: subscription_1.id, status: 0)
+
+      expect(customer_1.active_subscriptions.count).to eq(1)
+
+      customer_subscription_1.update(status: 1)
+
+      expect(customer_1.active_subscriptions.count).to eq(0)
+    end
+
+    it 'can give cancelled subscriptions' do
+      customer_1 = Customer.create!(first_name: 'Jim', last_name: 'Bob', email: 'jimmmybob@email.com', address: 'Springfield')
+      subscription_1 = Subscription.create!(title: 'Monthly', price: 10, frequency: 1)
+
+      expect(customer_1.cancelled_subscriptions.count).to eq(0)
+
+      customer_subscription_1 = CustomerSubscription.create!(customer_id: customer_1.id, subscription_id: subscription_1.id, status: 0)
+
+      expect(customer_1.cancelled_subscriptions.count).to eq(0)
+
+      customer_subscription_1.update(status: 1)
+
+      expect(customer_1.cancelled_subscriptions.count).to eq(1)
+    end
+  end
 end
