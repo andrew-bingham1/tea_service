006ba5bab5e87fa41bd3f2eae27e9bc912827d77
Andrew Bingham <andrewgbingham@gmail.com>
2023-08-03 08:11:44 -0600

    feat: create route for customer data

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api do
     namespace :v0 do
       resources :customer_subscriptions, only: [:create]
       delete '/customer_subscriptions', to: 'customer_subscriptions#destroy'
+      get '/customers', to: 'customers#show'
     end
   end
 end
