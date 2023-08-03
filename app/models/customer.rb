316a6c1411054cf39dcfd455b61d1a32b9aeceb4
Andrew Bingham <andrewgbingham@gmail.com>
2023-08-03 08:12:33 -0600

    feat: create active and cancelled sub methods for customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,12 @@
 class Customer < ApplicationRecord 
   has_many :customer_subscriptions
   has_many :subscriptions, through: :customer_subscriptions
+
+  def active_subscriptions
+    subscriptions.joins(:customer_subscriptions).where('customer_subscriptions.status = ?', 0)
+  end
+
+  def cancelled_subscriptions
+    subscriptions.joins(:customer_subscriptions).where('customer_subscriptions.status = ?', 1)
+  end
 end
